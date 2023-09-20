df5f5c3bd0a6b7d377daaf5074014d561e44d8a3
Raphael Zimeray <raphaelzimeray@me.com>
2023-09-20 18:05:11 +0200

    done

--- src/app/pianoindex/pianoindex.component.ts
+++ src/app/pianoindex/pianoindex.component.ts
@@ -3,11 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-pianoindex',
   templateUrl: './pianoindex.component.html',
-  styleUrls: ['./pianoindex.component.scss']
+  styleUrls: ['./pianoindex.component.scss'],
 })
 export class PianoindexComponent {
-  pianos = null ;
-  selectedvalue ='default';
+  pianos = null;
+  selectedvalue = 'default';
   search = '';
   constructor() {
     console.log('Etat 1', this.pianos);
@@ -21,8 +21,26 @@ export class PianoindexComponent {
 
   filterPianos() {
     // @ts-expect-error
-    return this.pianos.filter((p) =>
-      p.name.toLocaleLowerCase().includes(this.search.toLocaleLowerCase())
-    );
+    return this.pianos
+      .filter((p:any) =>
+        p.name.toLocaleLowerCase().includes(this.search.toLocaleLowerCase())
+      )
+      .sort( (a: any, b: any) => {
+        if(this.selectedvalue === 'mostexpensive'){
+          return b.price - a.price;
+        }
+        else if(this.selectedvalue === 'lessexpensive'){
+          return a.price - b.price;
+        }
+        else if(this.selectedvalue === 'alphabet'){
+          return b.name < a.name? 1:-1;
+        }
+        else if(this.selectedvalue === 'alphabetdesc'){
+          return a.name < b.name? 1:-1;
+        }
+        else{
+          return a.id - b.id;
+        }
+      });
   }
 }
